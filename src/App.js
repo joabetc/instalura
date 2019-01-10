c57fa0c0c5d82eb1acf78f3e7edf758494ece640
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2019-01-10 13:16:09 -0200

    Removing unused and importing html code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,103 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div id="root">
+      <div className="main">
+
+         <header className="header container">
+          <h1 className="header-logo">
+            Instalura
+          </h1>
+
+           <form className="header-busca">
+            <input type="text" name="search" placeholder="Pesquisa" className="header-busca-campo"/>
+            <input type="submit" value="Buscar" className="header-busca-submit"/>
+          </form>
+
+
+           <nav>
+            <ul className="header-nav">
+              <li className="header-nav-item">
+                <a href="#">
+                  ♡
+                  {/*                 ♥ */}
+                  {/* Quem deu like nas minhas fotos */}
+                </a>
+              </li>
+            </ul>
+          </nav>
         </header>
+
+         <div className="fotos container">
+          <div className="foto">
+            <header className="foto-header">
+              <figure className="foto-usuario">
+                <img src="https://cdn.dribbble.com/users/75924/avatars/mini/d46dc6f0108af6ed345bf7fa274a748b.png?1440271468" alt="foto do usuario"/>
+                <figcaption className="foto-usuario">
+                  <a href="#">
+                    alots
+                  </a>  
+                </figcaption>
+              </figure>
+              <time className="foto-data">03/10/2016 20:13</time>
+            </header>
+
+             <img alt="foto" className="foto-src" src="https://images.pexels.com/photos/373912/pexels-photo-373912.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"/>
+
+             <div className="foto-info">
+              <div className="foto-info-likes">
+
+                 <a href="#">
+                  alots_ssa
+                </a>
+
+                 ,
+
+                 <a href="#">
+                  rafael_rollo
+                </a> 
+
+                  curtiram
+
+               </div>
+
+               <p className="foto-info-legenda">
+                <a className="foto-info-autor">autor </a>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est, illo?
+              </p>
+
+               <ul className="foto-info-comentarios">
+                <li className="comentario">
+                  <a className="foto-info-autor">seguidor </a>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem ad, molestiae.
+                </li>
+                <li className="comentario">
+                  <a className="foto-info-autor">seguidor </a>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt cumque earum molestias voluptatem modi nihil sit magnam ratione eveniet distinctio magni error asperiores dignissimos tempora expedita, laborum ex soluta hic maiores veritatis deserunt.
+                </li>
+                <li className="comentario">
+                  <a className="foto-info-autor">seguidor </a>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum laudantium quae ab fuga odio delectus maiores voluptatibus sit commodi quidem.
+                </li>
+              </ul>
+            </div>
+
+
+             <section className="fotoAtualizacoes">
+              <a href="#" className="fotoAtualizacoes-like">Likar</a>
+              <form className="fotoAtualizacoes-form">
+                <input type="text" placeholder="Adicione um comentário..." className="fotoAtualizacoes-form-campo"/>
+                <input type="submit" value="Comentar!" className="fotoAtualizacoes-form-submit"/>
+              </form>
+
+             </section>
+          </div>
+
+         </div>
       </div>
+    </div>
     );
   }
 }
