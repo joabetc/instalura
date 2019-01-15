27bc2f1713b003e1821e3178fdb7aa29077a96d0
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2019-01-15 12:55:59 -0200

    Using Fetch API

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -3,10 +3,26 @@ import PhotoItem from './PhotoItem';
 
 export default class Timeline extends Component {
 
+  constructor() {
+    super();
+    this.state = { photos: [] };
+  }
+  componentDidMount() {
+    fetch('https://instalura-api.herokuapp.com/api/public/fotos/rafael')
+      .then(response => response.json())
+      .then(photos => {
+        this.setState({
+          photos: photos
+        });
+      });
+  }
+
   render() {
     return (
       <div className="fotos container">
-        <PhotoItem/>
+        {
+          this.state.photos.map(photo => <PhotoItem photo={photo}/>)
+        }
       </div>
     );
   }
