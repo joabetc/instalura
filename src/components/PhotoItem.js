23c4dbb474caceedd532f2d5f5eab4ef4fe16e7f
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2019-01-15 13:09:44 -0200

    Filling PhotoItem component with data

--- src/components/PhotoItem.js
+++ src/components/PhotoItem.js
@@ -21,33 +21,29 @@ class PhotoInfo extends Component {
     return (
       <div className="foto-info">
         <div className="foto-info-likes">
-          <a href="#">
-            alots_ssa
-          </a>
-          ,
-          <a href="#">
-            rafael_rollo
-          </a> 
+          {
+            this.props.photo.likers.map(liker => {
+              return (<a href="#">{liker.login},</a>);
+            })
+          }
           curtiram
         </div>
         <p className="foto-info-legenda">
           <a className="foto-info-autor">autor </a>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est, illo?
+          {this.props.photo.cometario}
         </p>
 
         <ul className="foto-info-comentarios">
-          <li className="comentario">
-            <a className="foto-info-autor">seguidor </a>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem ad, molestiae.
-          </li>
-          <li className="comentario">
-            <a className="foto-info-autor">seguidor </a>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt cumque earum molestias voluptatem modi nihil sit magnam ratione eveniet distinctio magni error asperiores dignissimos tempora expedita, laborum ex soluta hic maiores veritatis deserunt.
-          </li>
-          <li className="comentario">
-            <a className="foto-info-autor">seguidor </a>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum laudantium quae ab fuga odio delectus maiores voluptatibus sit commodi quidem.
-          </li>
+          {
+            this.props.photo.comentarios.map(comentario => {
+              return (
+                <li className="comentario">
+                  <a className="foto-info-autor">{comentario.login} </a>
+                  {comentario.texto}
+                </li>
+              )
+            })
+          }
         </ul>
       </div>
     );
@@ -81,7 +77,7 @@ export default class PhotoItem extends Component {
 
           <img alt="foto" className="foto-src" src={this.props.photo.urlFoto}/>
 
-          <PhotoInfo/>
+          <PhotoInfo photo={this.props.photo}/>
 
           <PhotoUpdates/>
         </div>
