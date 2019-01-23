39ccdbc56cda4a4ece0138025a8dfd0e74d5269c
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2019-01-23 12:32:55 -0200

    Authentication through API

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,13 +2,46 @@ import React, { Component } from 'react';
 
 export default class Login extends Component {
 
+  constructor() {
+    super();
+    this.state = {message: ''};
+  }
+
+  send(event) {
+    event.prevendDefault();
+
+    const requestInfo = {
+      method: 'POST',
+      body: JSON.stringify({
+        login: this.login.value,
+        senha: this.password.value
+      }),
+      headers: new Headers({
+        'Contant-type': 'application/json'
+      })
+    };
+
+    fetch('https://instalura-api.herokuapp.com/api/public/login', requestInfo)
+      .then(response => {
+        if (response.ok) {
+          return response.text()
+        } else {
+          this.setState({message: 'It was not possible to login!'})
+        }
+      })
+      .then(token => {
+        console.log(token);
+      })
+  }
+
   render() {
     return (
       <div className="login-box">
         <h1 className="header-top">Instalura</h1>
-        <form>
-          <input type="text"/>
-          <input type="password"/>
+        <span>{this.state.message}</span>
+        <form onSubmit={this.send.bind(this)}>
+          <input type="text" ref={(input) => this.login = input}/>
+          <input type="password" ref={(input) => this.password = input}/>
           <input type="submit" value="Login"/>
         </form>
       </div>
