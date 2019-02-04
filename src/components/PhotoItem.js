e1defd97cfb71ad172488b49d8219554cc116288
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2019-02-04 13:18:28 -0200

    Implementing commet feature

--- src/components/PhotoItem.js
+++ src/components/PhotoItem.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 import { Pubsub } from 'pubsub-js';
+import Header from './Header';
 
 class PhotoUpdates extends Component {
 
@@ -23,7 +24,31 @@ class PhotoUpdates extends Component {
       }
     }).then(liker => {
       this.setState({ liked: !this.state.liked });
-      Pubsub.publish('update-liker', {photoId: this.props.photo.id, liker });
+      Pubsub.publish('update-liker', { photoId: this.props.photo.id, liker });
+    });
+  }
+
+  createComment(event) {
+    event.preventDefault();
+
+    const requestInfo = {
+      method: 'POST',
+      body: JSON.stringify({ texto: this.comment.value }),
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      })
+    }
+
+    fetch(`https://instalura-api.herokuapp.com/api/public/fotos/${this.props.photo.id}/comment?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`,
+        requestInfo
+    ).then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('It was not possible to comment!');
+      }
+    }).then(comment => {
+      Pubsub.publish('new-comments' , { photoId: this.props.photo.id, comment })
     });
   }
 
@@ -31,8 +56,8 @@ class PhotoUpdates extends Component {
     return (
       <section className="fotoAtualizacoes">
         <a onClick={this.like.bind(this)} href="#" className={this.state.liked ? 'fotoAtualizacoes-like-ativo' : 'fotoAtualizacoes-like'}>Likar</a>
-        <form className="fotoAtualizacoes-form">
-          <input type="text" placeholder="Adicione um comentário..." className="fotoAtualizacoes-form-campo"/>
+        <form className="fotoAtualizacoes-form" onSubmit={this.createComment.bind(this)}>
+          <input type="text" placeholder="Adicione um comentário..." className="fotoAtualizacoes-form-campo" ref={input => this.comment = input}/>
           <input type="submit" value="Comentar!" className="fotoAtualizacoes-form-submit"/>
         </form>
       </section>
@@ -44,10 +69,13 @@ class PhotoInfo extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { likers: this.props.photo.likers };
+    this.state = { 
+      likers: this.props.photo.likers,
+      comments: this.props.photo.comments
+    };
   }
 
-  componentDidMount() {
+  componentWillMount() {
     Pubsub.subscribe('update-liker', (topic, likerInfo) => {
       if (this.props.photo.id === likerInfo.photoId) {
         const possibleLiker = this.state.likers.find(liker => liker.login === likerInfo.liker.login);
@@ -58,6 +86,13 @@ class PhotoInfo extends Component {
           const newLikers = this.state.likers.filter(liker => liker.login !== likerInfo.liker.login);
           this.setState({likers: newLikers});
         }
+      }
+    });
+
+    Pubsub.subscribe('new-comments', (topic, commentInfo) => {
+      if (this.props.photo.id === commentInfo.photoId) {
+        const newComments = this.state.comments.concat(commentInfo.comment);
+        this.setState({comments: newComments});
       }
     });
   }
@@ -80,7 +115,7 @@ class PhotoInfo extends Component {
 
         <ul className="foto-info-comentarios">
           {
-            this.props.photo.comentarios.map(comment => {
+            this.state.comments.map(comment => {
               return (
                 <li className="comentario" key={comment.id}>
                   <Link to={`/timeline/${comment.login}`} className="foto-info-autor">{comment.login}</Link>
